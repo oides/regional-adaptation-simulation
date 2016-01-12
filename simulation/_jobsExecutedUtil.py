b20052738977ca7be210b5106272a4e87cf011d4
Eudes Santos Andrade <eudes.andrade@serpro.gov.br>
2016-01-12 09:46:36 -0300

    Obtendo o número de jobs executados em cada execução da simulação.

---
+++ simulation/_jobsExecutedUtil.py
@@ -0,0 +1,22 @@
+"""
+Utilitário para suporte a execução da simulacao.
+
+"""
+__author__ = 'Eudes Santos Andrade'
+
+
+import yaml
+
+f = open('generatedreport.yaml', 'r')
+
+report = yaml.load(f)
+
+JOBS_EXECUTED = report['jobsExecutedOnNodes']
+totalJobs = 0
+
+for node in JOBS_EXECUTED:
+    totalJobs += node.get('jobsExecuted')
+
+print(str(totalJobs))
+
+
--- simulation/executeSimulation.sh
+++ simulation/executeSimulation.sh
@@ -1,1 +1,8 @@
+# Executing simulation
 python overheadsimulation.py
+
+# Geting jobs number executed
+va=$(python _jobsExecutedUtil.py)
+
+
+echo 'Jobs number executed: ' $va
